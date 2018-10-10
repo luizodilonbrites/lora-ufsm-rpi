e30e18cc85eb0627b94927f6bea8adb5843ed470
Luiz Odilon de Lima Brites <luizbrites@hotmail.com>
2018-10-10 16:31:05 -0300

    Update ttn-abp-send.cpp
    Now is possible read data from file directly.

--- examples/ttn-abp-send/ttn-abp-send.cpp
+++ examples/ttn-abp-send/ttn-abp-send.cpp
@@ -8,7 +8,8 @@
 ***************************************************************************
   Version: 1.0
   Date: 27/06/2018
-  Modified: --/--/----
+  Modified: 09/10/2018 by Luiz Odilon de Lima Brites
+  Added functionality in reading DevAddr, Nwskey and Appskey from file
 ***************************************************************************
   Copyright(c) by: Fox IoT.
 **************************************************************************/
@@ -28,6 +29,11 @@ CONFIGURATION NODE LORA
 #include <lmic.h>
 #include <hal.h>
 #include <local_hal.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
 
 /**************************************************************************
   VARIABLES AND DEFINITIONS
@@ -35,20 +41,31 @@ CONFIGURATION NODE LORA
 // Module RFM95 pin mapping
 #define RFM95_PIN_NSS 6
 #define RFM95_PIN_RST 0
-#define RFM95_PIN_D0  4 
-#define RFM95_PIN_D1  5 
+#define RFM95_PIN_D0  4
+#define RFM95_PIN_D1  5
 //#define RFM95_PIN_D2 1
 
 #define STATUS_PIN_LED 2
 
+
+/*************************************************************************
+  DECLARATION FOR READING DATA FROM FILE - BY BRITES, L. O. L
+/************************************************************************/
+FILE *fp;
+char c, str1[2], str2[4];
+int i = 0, j = 0, k = 0, l = 0;
+int DevAddr_a[4], Nwkskey_a[16], Appskey_a[16];
+/************************************************************************
+  END DECLARATION Added and code continues
+/***********************************************************************/
 // LoRaWAN end-device address (DevAddr)
-static const u1_t DevAddr[4] ={0x26, 0x01, 0x17, 0x2E};
+u1_t DevAddr[4]; //={0x26, 0x01, 0x17, 0x2E};
 
 // LoRaWAN NwkSKey, network session key
-static const u1_t Nwkskey[16] ={0x81, 0x58, 0xC3, 0x00, 0xAB, 0x35, 0x5E, 0xD7, 0xF4, 0x1D, 0xC9, 0x0F, 0xDD, 0x4C, 0xB1, 0xB9};
+u1_t Nwkskey[16]; //{0x81, 0x58, 0xC3, 0x00, 0xAB, 0x35, 0x5E, 0xD7, 0xF4, 0x1D, 0xC9, 0x0F, 0xDD, 0x4C, 0xB1, 0xB9};
 
 // LoRaWAN AppSKey, application session key
-static const u1_t Appskey[16] ={0xB7, 0xA2, 0x76, 0xBE, 0xAB, 0x79, 0xDF, 0x9C, 0xA0, 0x7B, 0xB6, 0xB4, 0x4A, 0xF5, 0x67, 0xE8};
+u1_t Appskey[16]; //{0xB7, 0xA2, 0x76, 0xBE, 0xAB, 0x79, 0xDF, 0x9C, 0xA0, 0x7B, 0xB6, 0xB4, 0x4A, 0xF5, 0x67, 0xE8};
 
 // Schedule TX every this many seconds (might become longer due to duty cycle limitations).
 int TX_INTERVAL = 30;
@@ -86,14 +103,14 @@ void onEvent(ev_t ev)
     // scheduled data sent (optionally data received)
     // note: this includes the receive window!
     case EV_TXCOMPLETE:
-      
+
       // use this event to keep track of actual transmissions
       fprintf(stdout, "Event EV_TXCOMPLETE, time: %d\n", millis() / 1000);
 
       // Check ACK
       if (LMIC.txrxFlags & TXRX_ACK) fprintf(stdout, "Received ACK!\n");
       else if (LMIC.txrxFlags & TXRX_NACK)  fprintf(stdout, "No ACK received!\n");
-    
+
       // Check DOWN
       if (LMIC.dataLen)
       {
@@ -126,7 +143,7 @@ static void do_send(osjob_t * j)
   fprintf(stdout, "[%x] (%ld) %s\n", hal_ticks(), t, ctime(& t));
   // Show TX channel (channel numbers are local to LMIC)
   // Check if there is not a current TX/RX job running
-  if (LMIC.opmode & OP_TXRXPEND) 
+  if (LMIC.opmode & OP_TXRXPEND)
   {
     fprintf(stdout, "OP_TXRXPEND, not sending");
   }
@@ -152,10 +169,10 @@ static void do_send(osjob_t * j)
 PI_THREAD (blinkRun)
 {
   while (1)
-  {  
-    if ((millis() % 3000) < 250) digitalWrite(STATUS_PIN_LED, HIGH);  
+  {
+    if ((millis() % 3000) < 250) digitalWrite(STATUS_PIN_LED, HIGH);
     else digitalWrite(STATUS_PIN_LED, LOW);
-  } 
+  }
 }
 */
 
@@ -163,18 +180,121 @@ void setup()
 {
   //wiringPi init
   wiringPiSetup();
-  
+
   // LMIC init
   os_init();
-  
+
   // Reset the MAC state. Session and pending data transfers will be discarded.
   LMIC_reset();
+
+/**************************************************************************
+  NEW FUNCTIONALITY FOR DATA FILE HANDLER
+/**************************************************************************/
+  fp=fopen("id_data.txt", "r");
+
+  c=getc(fp);
+  while(i < 1){
+    sprintf(str2,"%s", "0x");
+    sprintf(str1, "%c", c);
+    strcat(str2, str1);
+    c=getc(fp);
+    sprintf(str1, "%c", c);
+    strcat(str2, str1);
+    c=getc(fp);
+    if(i == 0 && j < 4){
+      DevAddr_a[j] = (int)strtol(str2, NULL, 0);
+      j++;
+    }
+    if(j == 4){
+      i++;
+      printf("\n=== line %d ok\n", i);
+      c=getc(fp);
+    }
+  }
+  for(i=0; i<4; i++){
+    DevAddr[i] = (u1_t)DevAddr_a[i];
+  }
+  for(i=0; i<4; i++){
+    printf("devaddr-%d===%x\n", i, DevAddr[i]);
+  }
+  i=0;
+  j=0;
+  c=getc(fp);
+  while(i < 1){
+    sprintf(str2,"%s", "0x");
+    sprintf(str1, "%c", c);
+    strcat(str2, str1);
+    c=getc(fp);
+    sprintf(str1, "%c", c);
+    strcat(str2, str1);
+    c=getc(fp);
+    if(i == 0 && j < 16){
+      Nwkskey_a[j] = (int)strtol(str2, NULL, 0);
+      j++;
+    }
+    if(j == 16){
+      i++;
+      printf("\n=== line %d ok 2 \n", i);
+      c=getc(fp);
+    }
+  }
+  for(i=0; i<16; i++){
+    Nwkskey[i] = (u1_t)Nwkskey_a[i];
+  }
+  for(i=0; i<16; i++){
+    printf("Nwkskey-%d===%x\n", i, Nwkskey[i]);
+  }
+  i=0;
+  j=0;
+  c=getc(fp);
+  while(i < 1){
+    sprintf(str2,"%s", "0x");
+    sprintf(str1, "%c", c);
+    strcat(str2, str1);
+    c=getc(fp);
+    sprintf(str1, "%c", c);
+    strcat(str2, str1);
+    c=getc(fp);
+    if(i == 0 && j < 16){
+      Appskey_a[j] = (int)strtol(str2, NULL, 0);
+      j++;
+    }
+    if(j == 16){
+      i++;
+      printf("\n=== line %d ok 3 \n", i);
+      c=getc(fp);
+    }
+  }
+
+  for(i=0; i<16; i++){
+    Appskey[i] = (u1_t)Appskey_a[i];
+  }
+
+  for(i=0; i<16; i++){
+    printf("Appskey-%d===%x\n", i, Appskey[i]);
+  }
+
+  for(i=0; i<4; i++){
+    printf("devaddr-%d===%x\n", i, DevAddr[i]);
+  }
+  for(i=0; i<16; i++){
+    printf("Nwkskey-%d===%x\n", i, Nwkskey[i]);
+  }
   
+  for(i=0; i<16; i++){
+    printf("Appskey-%d===%x\n", i, Appskey[i]);
+  }
+  fclose(fp);
+  printf("\nCopied from file in read operation!==========By Luiz Odilon\n");
+/***************************************************************************
+    END HANDLER OPERATIONS AND THE CODE continues
+/**************************************************************************/
+
   // Set static session parameters. Instead of dynamically establishing a session
   // by joining the network, precomputed session parameters are be provided.
   LMIC_setSession(SESSION_PORT, msbf4_read((u1_t *)DevAddr),(u1_t *)Nwkskey,(u1_t *)Appskey);  /*
 
-/* 
+/*
   // Multi channel AU915 (CH0-CH7)
   // First, disable channels 0-7
   for (int channel = 0; channel < 8; ++channel)
@@ -184,7 +304,7 @@ void setup()
     LMIC_disableChannel(channel);
   // This means only channels 8-15 are up
 */
- 
+
   // Single channel AU915 (CH0)
   //First, disable channels 0-7
   for (int channel = 0; channel < 8; ++channel) LMIC_disableChannel(channel);
@@ -194,25 +314,25 @@ void setup()
 
   // Disable data rate adaptation
   LMIC_setAdrMode(0);
-  
+
   // Disable link check validation
   LMIC_setLinkCheckMode(0);
-  
+
   // Disable beacon tracking
   LMIC_disableTracking();
-  
+
   // Stop listening for downstream data (periodical reception)
   LMIC_stopPingable();
-  
+
   // TTN RX2 window.
   LMIC.dn2Dr = DATA_RATE_UP_DOWN;
-  
+
   // Set data rate and transmit power (note: txpow seems to be ignored by the library)
   LMIC_setDrTxpow(DATA_RATE_UP_DOWN, TX_POWER);
 
   // Set pin direction
   pinMode(STATUS_PIN_LED, OUTPUT);
-  
+
   //Add thread
   //piThreadCreate (blinkRun);
 
@@ -230,17 +350,17 @@ int main()
   setup();
   while (1)
   {
-   loop(); 
+   loop();
   }
   return 0;
 }
 
 // TTN decode payload
 /*
-function Decoder(bytes, port) 
+function Decoder(bytes, port)
 {
   var result = "";
-  for (var i = 0; i < bytes.length; i++) 
+  for (var i = 0; i < bytes.length; i++)
   {
     result += (String.fromCharCode(bytes[i]));
   }
